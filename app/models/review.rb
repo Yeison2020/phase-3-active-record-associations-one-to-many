2a6d4281edbe67a6f058d46f9bc7816fd15c3590
Yeison <enmanuelquiterios@gmail.com>
2021-11-07 21:16:04 -0500

    commit tree 3d73c31241e27ba33834e40e8d0e97c1e56d2d15

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,10 @@
 class Review < ActiveRecord::Base
-  
+#   def game 
+#     Game.find(self.game_id)
+
+#   end
+# belongs_to( :game)
+# Note here the belongs_to is not plural while has_jmany It is.
+belongs_to :game
+
 end
